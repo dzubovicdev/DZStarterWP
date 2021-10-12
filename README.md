9a4d94da11d60bd2d92c322dc6fc03c084a3cd6d
Damir Zubovic <damir@dzubovic.com>
2021-10-12 21:36:12 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# [WordPress Theme - DZStarterWP 🎨
+[![Project Status: Active.]](https://www.repostatus.org/#active)
+* A WordPress theme Project for starter theme.
+This theme uses Bootstrap. 
+
+- Custom front page.
+- Custom Blog page with posts displayed in grid format using bootstrap.
+- Block Style Variations
+- Custom Gutenberg Blocks
+- InnerBlocks
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Main template file.
+ *
+ * @package DZStarterWP
+ */
+
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>DZStarterWP - WordPress Starter Theme</title>
+ </head>
+ <body>
+    <header>Header</header>
+    <footer>Footer</footer>
+ </body>
+ </html>
